1b9bae2405240f2652a328f49535e7345cae3348
NadaYousry <nada26801995@gmail.com>
2020-03-01 17:20:41 +0200

    edit profile

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -12,80 +12,98 @@ import { users } from '../modals/users';
 export class ProfileComponent implements OnInit {
 
 
+
+
+
+
+  // **************************** start  vars ******************************//
+  constructor(private f: FormBuilder,
+    private usersService: usersService) { }
+  userComment: FormGroup;
+  userProfile: FormGroup;
+  colors;
+  borderLeft;
+  randomColor;
+  fileData: any;
+  fileSrc: string | ArrayBuffer;
+  file: any;
+  profile: users;
+
+  sum: number;
+  avgStars = 0;
+  userSum = 0;
+
+  // ************* end  vars ***************//
+
+
+
+
+
   // ************* start form ***************//
-  usersComments = [
+  usersComments = [];
+  editUserProfile = [
     {
-      img: '../assets/imgs/users/default-user-image-300x300.png',
-      name: 'nada',
-      comment: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, mollitiaLorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, mollitiaLorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, mollitia',
+      img: '',
+      name: '',
+      Bio: ''
     }
   ];
-
-  constructor(private f: FormBuilder,
-    private usersService:usersService) { }
-    userComment: FormGroup;
-    userProfile: FormGroup;
-    colors;
-    borderLeft;
-    randomColor;
-    fileData: any;
-    fileSrc: string | ArrayBuffer;
-    file: any;
-    profile:users;  
-    commentStars=[];
-    avgStars:number;
-
-
+  currentUserProfile = {
+    name: 'Nada Yousry',
+    title: 'Front-End Developer',
+    image: '',
+    starts: '',
+    email: 'nadayousry@gmail.com',
+    comments: ''
+  }
   ngOnInit() {
-    this.fileSrc="../../assets/imgs/users/default-user-image-300x300.png";
+    this.fileSrc = "../../assets/imgs/users/default-user-image-300x300.png";
     this.userComment = this.f.group({
       img: '../assets/imgs/users/default-user-image-300x300.png',
       name: 'nada',
       comment: ['', [Validators.required]],
-      rate:''
+      rate: 0
     });
-    this.userProfile=this.f.group({
-      image:'',
-      starts:'',
-      comments:''
+    this.userProfile = this.f.group({
+      name: 'Nada Yousry',
+      title: 'Front-End Developer',
+      image: '',
+      starts: '',
+      email: '',
+      comments: '',
     });
   }
 
-sum=0;
   onSubmit(form: FormGroup) {
     if (form.valid) {
-      this.userComment.value.rate  = this.rating
+      this.sum = 0;
+      this.userComment.value.rate = this.rating //initialize rating on form submit 
       this.usersComments.push(this.userComment.value);
-      this.userProfile.value.comments = this.usersComments;
-      // this.userProfile.value.starts = this.rating;
-      this.commentStars.push(this.rating);
-      console.log(this.commentStars);
-      for(let i of this.commentStars){
-        this.sum=this.sum+this.commentStars[i];
-        console.log(this.sum);
-        
-        this.avgStars=this.sum/this.commentStars.length;
-      }
-      console.log(this.avgStars);
-      console.log(this.userProfile.value);
+      console.log(this.userComment.value);
+      console.log(this.usersComments);
       console.log("valid");
-       
+      //calc avg
+      for (let i = 0; i < this.usersComments.length; i++) {
+        this.sum += this.usersComments[i].rate;
+        this.avgStars = this.sum / this.usersComments.length;
+        console.log(this.usersComments[i].rate);
+      }
+
     }
+
+
     // ************* start border coloring ***************//
 
     this.colors = ['#00ca5d', '#2ca6ef', '#192965'];
     this.randomColor = Math.floor(Math.random() * 3);
-    console.log(this.colors[this.randomColor]);
+    // console.log(this.colors[this.randomColor]);
     this.borderLeft = "3px solid" + this.colors[this.randomColor];
+
     // ************* end border coloring ***************//
 
 
   }
   // ************* end form ***************//
-  onBtnSubmit(created){
-    console.log({created},created.childNodes[1]);
-    
-  }
 
 
   // ************* start star rating ***************//
@@ -102,18 +120,21 @@ sum=0;
   }
   onStarClicked(starId: number, dataHovering) {
     this.rating = starId;
-    dataHovering.style.top='95%';
-    setTimeout(()=>{  
-    dataHovering.style.top='100%';
-    },1000)
-    console.log(dataHovering);
+    dataHovering.style.top = '95%';
+    setTimeout(() => {
+      dataHovering.style.top = '100%';
+    }, 1000)
+    // console.log(dataHovering);
+
 
   }
+
+
   // ************* end star rating ***************//
 
   readURL(event: any) {
     this.fileData = <File>event.target.files[0];
-    console.log(this.fileData);
+    // console.log(this.fileData);
     this.preview();
   }
 
@@ -128,10 +149,10 @@ sum=0;
     reader.onload = event => {
       this.fileSrc = reader.result;
       this.file = reader.result;
-      console.log(this.file)
+      // console.log(this.file)
       // this.users.value.attachment = this.file;
       // console.log(this.projectForm.value.attachment) ;
-      this.userProfile.value.image=this.file;
+      this.userProfile.value.image = this.file;
     };
   }
 }

a98501a1458824dc4f847b53f7de42596ee60f2c
Ali-MEmam <ali.emamfe@gmail.com>
2020-03-30 05:43:53 +0200

    console disbled

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -43,7 +43,7 @@ export class LoginComponent implements OnInit {
 
     this.userData.getUser().subscribe(users => {
       this.allUsers = users;
-      console.log(this.allUsers)
+      // console.log(this.allUsers)
     })
 
     this.login = this.fb.group({
